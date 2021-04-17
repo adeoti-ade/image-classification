9546d3fe83e63d570ffdc7b60b55c0e0f7b1b8e3
Adeoti Adegboyega <adeotiadegboyega@gmail.com>
2021-04-17 05:34:13 +0100

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mac/Documents/Projects/web-dev/mideweight_backend_test/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
