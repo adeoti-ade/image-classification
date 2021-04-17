7405e7366427bad762a9b25ded1addc4a085f917
Adeoti Adegboyega <adeotiadegboyega@gmail.com>
2021-04-17 06:29:05 +0100

    added the user management app and added it's model

---
+++ src/users/models.py
@@ -0,0 +1,78 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+from safedelete.models import SafeDeleteModel
+from safedelete.models import SOFT_DELETE_CASCADE
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, SafeDeleteModel):
+    """
+    This is a custom user model that inherits the abstract base user and safe delete mode
+    """
+    _safedelete_policy = SOFT_DELETE_CASCADE
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    active = models.BooleanField(default=False)
+    staff = models.BooleanField(default=False)  # a admin user; non super-user
+    admin = models.BooleanField(default=False)  # a superuser
+    # notice the absence of a "Password field", that is built in.
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # Email & Password are required by default.python manage.py migrate
+
+    objects = UserManager()
+
+    @property
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    @property
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        return self.staff
+
+    @property
+    def is_admin(self):
+        """Is the user a admin member?"""
+        return self.admin
+
+    @property
+    def is_active(self):
+        """Is the user active?"""
+        return self.active
