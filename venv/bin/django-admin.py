2ed4e7c8720e8056522738c2c340b28e22a26e4c
Adeoti Adegboyega <adeotiadegboyega@gmail.com>
2021-04-17 05:35:21 +0100

    removed the virtual envinronment module

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mac/Documents/Projects/web-dev/mideweight_backend_test/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
