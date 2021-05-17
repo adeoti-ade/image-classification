a02411971270f34e0adc9f7ca89fad797b5944d7
Adeoti Adegboyega <adeotiadegboyega@gmail.com>
2021-05-17 09:59:41 +0100

    created a transaction model

--- src/wallets/models.py
+++ src/wallets/models.py
@@ -10,7 +10,7 @@ class Wallet(BaseModel):
     """
     This is the class that defines the wallet table schema
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wallet")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="wallet")
     balance = models.DecimalField(max_digits=100, decimal_places=2, null=False, default=0.00)
 
     class Meta:
@@ -18,5 +18,21 @@ class Wallet(BaseModel):
 
     def __str__(self):
         return self.user.email
+
+
+class Transaction(BaseModel):
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name="transactions")
+    narration = models.CharField(max_length=255, null=True)
+    credit = models.DecimalField(max_digits=100, decimal_places=2, null=False, default=0.00)
+    debit = models.DecimalField(max_digits=100, decimal_places=2, null=False, default=0.00)
+    balance = models.DecimalField(max_digits=100, decimal_places=2, null=False, default=0.00)
+
+    class Meta:
+        ordering = ("-created", )
+
+    def __str__(self):
+        return self.wallet
+
+
 
 
